7d193e91ad8fcec2c3b4dead5035b2423989e4df
msgilligan <devnull@localhost>
2009-10-20 22:58:09 -0700

    add comment explaining GAE Java SDK must be installed

--- runWebApp.sh
+++ runWebApp.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# To use this script install the Google App Engine SDK for Java in the directory
+# /usr/local/appengine-java-sdk
 /usr/local/appengine-java-sdk/bin/dev_appserver.sh --address=0.0.0.0 web-app
