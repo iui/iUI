97682e352a27dcf5fe0e716a288c970d2f383b2d
Remi Grumeau <devnull@localhost>
2011-04-10 17:40:17 +0200

    Coherance - modifications - can be revert

--- web-app/samples/music/search.gtpl
+++ web-app/samples/music/search.gtpl
@@ -1,5 +1,4 @@
 <ul title="Search Results">
     <li><a href="#player">Song (by $params.artist)</a></li>
-    <li><a href="#player">Song (by $params.artist)</a></li>
-    <li><a href="#player">Song (by $params.artist)</a></li>
+    <li><a href="#player">Album (by $params.artist)</a></li>
 </ul>
--- web-app/samples/music/search.php
+++ web-app/samples/music/search.php
@@ -1,5 +1,4 @@
 <ul title="Search Results">
-    <li><a href="#player">Song (by <?php echo $_POST['artist']; ?>)</a></li>
-    <li><a href="#player">Song (by <?php echo $_POST['artist']; ?>)</a></li>
-    <li><a href="#player">Song (by <?php echo $_POST['artist']; ?>)</a></li>
+    <li><a href="#player">Songs (by <?php echo $_POST['artist']; ?>)</a></li>
+    <li><a href="#player">Albums (by <?php echo $_POST['artist']; ?>)</a></li>
 </ul>
