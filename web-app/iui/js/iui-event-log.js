9998287de669189e17b92e5aec332d1aca25744f
Remi Grumeau <devnull@localhost>
2011-04-10 17:47:34 +0200

    Coherance - modifications - can be revert

--- web-app/iui/js/iui-event-log.js
+++ web-app/iui/js/iui-event-log.js
@@ -21,9 +21,9 @@ addEventListener("DOMContentLoaded", function(event)
 {
 	document.body.addEventListener('beforeinsert', logEvent, false);
 	document.body.addEventListener('afterinsert', afterInsert, false);
-// This will register event handlers on all initial nodes
-// We'll also need to register handlers on inserted (via ajax) nodes
-// To do that we'll need to use the afterInsert event
+	// This will register event handlers on all initial nodes
+	// We'll also need to register handlers on inserted (via ajax) nodes
+	// To do that we'll need to use the afterInsert event
 	var nodes = iui.getAllViews();
 	for (var i = 0; i  < nodes.length  ; i++)
 	{
