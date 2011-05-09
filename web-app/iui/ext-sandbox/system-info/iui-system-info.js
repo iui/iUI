bde6382aebb84b7cd098f740b92ca5b110bd1e42
Remi Grumeau <devnull@localhost>
2011-05-09 02:23:36 +0200

    system info v1.1 - add Bada - add Blackberry - add Playbook - bugfixes

--- web-app/iui/ext-sandbox/system-info/iui-system-info.js
+++ web-app/iui/ext-sandbox/system-info/iui-system-info.js
@@ -2,7 +2,7 @@
    Copyright (c) 2007-9, iUI Project Members
    See LICENSE.txt for licensing terms
    ************
-   LAST UDPATE: Oct 31th, 2010 - remi.grumeau@gmail.com
+   LAST UDPATE: May 9th, 2011 - remi.grumeau@gmail.com
    ************
 */
 
@@ -18,13 +18,23 @@ iui.sysinfo = {
 
 	osInfo : function()
 	{
-		if(iui.sysinfo.ua.match('iphone') || iui.sysinfo.ua.match('ipad')) {
-			iui.sysinfo.os.name	= 'iphoneos';
+		if(iui.sysinfo.ua.match('iphone') || iui.sysinfo.ua.match('ipod') || iui.sysinfo.ua.match('ipad')) {
+			iui.sysinfo.os.name	= 'ios';
 			iui.sysinfo.os.version = (/((\d+_){1,3}(\d+))/.test(iui.sysinfo.ua))?new String(RegExp.$1.replace('_','.')):'unknow';
 		}
 		else if(iui.sysinfo.ua.match('android')) {
 			iui.sysinfo.os.name	= 'android';
 			iui.sysinfo.os.version	= (/android ((\d+.){1,3}(\d+))/.test(iui.sysinfo.ua))?new String(RegExp.$1):'unknow';
+		}
+		else if(iui.sysinfo.ua.match('blackberry') || iui.sysinfo.ua.match('playbook')) {
+			iui.sysinfo.os.name	= (iui.sysinfo.ua.match('playbook'))?'playbook':'blackberry';
+			
+			if(/version\/((\d+.){1,2}(\d+))/.test(iui.sysinfo.ua))
+				iui.sysinfo.os.version = new String(RegExp.$1);
+			else if(/\/((\d+.){1,2}(\d+))/.test(iui.sysinfo.ua))
+				iui.sysinfo.os.version = new String(RegExp.$1);
+			else
+				iui.sysinfo.os.version = 'unknow';
 		}
 		else if(iui.sysinfo.ua.match('maemo')) {
 			iui.sysinfo.os.name	= 'maemo';
@@ -34,14 +44,20 @@ iui.sysinfo = {
 			iui.sysinfo.os.name	= 'webos';
 			iui.sysinfo.os.version = (/webos\/((\d+.){1,3})/.test(iui.sysinfo.ua))?new String(RegExp.$1):'unknow';
 		}
+		else if(iui.sysinfo.ua.match('bada')) {
+			iui.sysinfo.os.name	= 'bada';
+			iui.sysinfo.os.version = (/bada\/((\d+.){1}(\d+))/.test(iui.sysinfo.ua))?new String(RegExp.$1):'unknow';
+		}
 		else if(iui.sysinfo.ua.match('mac os x')) {
 			iui.sysinfo.os.name	= 'macos';
 			iui.sysinfo.os.version = (/((\d+_){1,3}(\d+))/.test(iui.sysinfo.ua))?new String(RegExp.$1.replace('_','.')):'unknow';
 		}
 		else if(iui.sysinfo.ua.match('windows')) {
-			iui.sysinfo.os.name	= 'windows';
+			iui.sysinfo.os.name	= (iui.sysinfo.ua.match('windows phone'))?'windows_phone':'windows';
 	
-			if(iui.sysinfo.ua.match('nt 5.1'))
+			if(/iemobile\/((\d+.){1,2}(\d+))/.test(iui.sysinfo.ua))
+				iui.sysinfo.os.version = new String(RegExp.$1);
+			else if(iui.sysinfo.ua.match('nt 5.1'))
 				iui.sysinfo.os.version	= 'xp';
 			else if(iui.sysinfo.ua.match('nt 5.2'))
 				iui.sysinfo.os.version	= '2003';
@@ -60,7 +76,7 @@ iui.sysinfo = {
 
 	deviceInfo: function() 
 	{
-		iui.sysinfo.device.touch = (typeof Touch == "object");
+		iui.sysinfo.device.touch = (typeof Touch == "object")?'true':'false';
 		
 		if(iui.sysinfo.ua.match('iphone')) {
 			iui.sysinfo.device.vendor 		= 'apple';
@@ -91,6 +107,18 @@ iui.sysinfo = {
 	
 			iui.sysinfo.device.type = (iui.sysinfo.ua.match('tablet'))?'tablet':'phone';
 		}
+		else if(iui.sysinfo.ua.match('blackberry')) {
+			iui.sysinfo.device.vendor 		= 'rim';
+			iui.sysinfo.device.type 		= 'phone';		
+		}
+		else if(iui.sysinfo.ua.match('playbook')) {
+			iui.sysinfo.device.vendor 		= 'rim';
+			iui.sysinfo.device.type 		= 'tablet';		
+		}
+		else if(iui.sysinfo.ua.match('bada')) {
+			iui.sysinfo.device.vendor 		= 'samsung';
+			iui.sysinfo.device.type 		= 'phone';		
+		}
 		else if(iui.sysinfo.ua.match('macintosh')) {
 			iui.sysinfo.device.vendor 		= 'apple';
 			iui.sysinfo.device.type 		= 'desktop';	
@@ -106,18 +134,34 @@ iui.sysinfo = {
 
 	browserInfo : function() 
 	{
-		iui.sysinfo.browser.webapp = (window.navigator.standalone==1)?'true':'false';
+		iui.sysinfo.browser.webapp = ("standalone" in window.navigator)?'true':'false';
 
-		if(iui.sysinfo.ua.match('chrome')) {
+		if(iui.sysinfo.ua.match('chrome')) 
+		{
 			iui.sysinfo.browser.name = 'chrome';
 			iui.sysinfo.browser.engine = 'webkit';
 			iui.sysinfo.browser.version	= (/version\/((\d+.){1,3}(\d+))/.test(iui.sysinfo.ua))?new String(RegExp.$1):'unknow';
 		}
-		else if(iui.sysinfo.ua.match('applewebkit') || (iui.sysinfo.ua.match('iphone')) || (iui.sysinfo.ua.match('safari'))) 
+		else if( iui.sysinfo.ua.match('iphone') || iui.sysinfo.ua.match('ipod') || iui.sysinfo.ua.match('ipad') )
 		{
 			iui.sysinfo.browser.name = 'safari';
 			iui.sysinfo.browser.engine = 'webkit';
 			iui.sysinfo.browser.version	= (/version\/((\d+.){1,3}(\d+))/.test(iui.sysinfo.ua))?new String(RegExp.$1):'unknow';
+		}
+		else if( iui.sysinfo.ua.match('blackberry') || iui.sysinfo.ua.match('playbook') ) {
+			iui.sysinfo.browser.name = 'blackberry';
+			iui.sysinfo.browser.engine = 'webkit';
+			if(/version\/((\d+.){1,2}(\d+))/.test(iui.sysinfo.ua))
+				iui.sysinfo.browser.version = new String(RegExp.$1);
+			else if(/\/((\d+.){1,2}(\d+))/.test(iui.sysinfo.ua))
+				iui.sysinfo.browser.version = new String(RegExp.$1);
+			else
+				iui.sysinfo.browser.version = 'unknow';
+		}
+		else if(iui.sysinfo.ua.match('bada')) {
+			iui.sysinfo.browser.name = 'dolfin';
+			iui.sysinfo.browser.engine = 'webkit';
+			iui.sysinfo.browser.version = (/dolfin\/((\d+.){1,3})/.test(iui.sysinfo.ua))?new String(RegExp.$1):'unknow';
 		}
 		else if(iui.sysinfo.ua.match('firefox') || iui.sysinfo.ua.match('fennec')) {
 			iui.sysinfo.browser.name = (iui.sysinfo.ua.match('fennec'))?'fennec':'firefox';
@@ -141,7 +185,7 @@ iui.sysinfo = {
 			iui.sysinfo.browser.version = window.navigator.appVersion;
 		}
 	
-		iui.sysinfo.browser.standalone = (window.navigator.standalone===true)?'true':'false';
+		iui.sysinfo.browser.standalone = (("standalone" in window.navigator) && !window.navigator.standalone)?'false':'true';
 	
 	},
 
