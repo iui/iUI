af862db979da0c4d5c388dcff3003abbf75bde2c
Sean Gilligan <sean@msgilligan.com>
2013-07-08 17:01:34 -0700

    Update node-deploy-cloudbees script for new account

--- node-deploy-cloudbees.sh
+++ node-deploy-cloudbees.sh
@@ -5,5 +5,5 @@ rm -f target/node-web-app.zip
 cd web-app
 zip -r ../target/node-web-app.zip *
 cd ..
-bees app:deploy --appid cmt/iui-demo-node -t node -RPLUGIN.SRC.node=https://s3.amazonaws.com/cloudbees-downloads/clickstack/node-clickstack.zip target/node-web-app.zip
+bees app:deploy --appid node-demo -t node -RPLUGIN.SRC.node=https://s3.amazonaws.com/cloudbees-downloads/clickstack/node-clickstack.zip target/node-web-app.zip
 
